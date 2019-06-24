f2d5408fd5a5fec72a71b62f4756b26117b0e427
Barun <barun.mittal@capgemini.com>
2019-06-24 16:06:52 +0100

    test

---
+++ site/profile/manifests/sftpplus/test.pp
@@ -0,0 +1,19 @@
+#test
+
+class profile::big::sftpplus::directory (
+  String $nexus_repo_AIX,
+) {
+
+  # Identify the Nexus Repository Server to be used.
+  $owner = lookup('profile::big::sftpplus::config::owner', String, deep)
+  $group = lookup('profile::big::sftpplus::config::owner', String, deep)
+
+
+  # Create the installation packages directory.
+  file { "/var/puppet/sftpplus":
+    ensure  => directory,
+    owner   => $owner,
+    group   => $group,
+    mode    => '0755',
+  }
+}
