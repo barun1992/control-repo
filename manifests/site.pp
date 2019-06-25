dcbce77d46d737506473fc2bb96088a01fa4af20
Barun <barun.mittal@capgemini.com>
2019-06-25 10:27:55 +0100

    update

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,21 +1,12 @@
 node 'puppetagent.master.com' {
-     file { '/etc/puppetlabs/facter':
-          ensure => 'directory',
-          owner  => 'root',
-          group  => 'root',
-          mode   => '0755',
-     }
-     file { '/etc/puppetlabs/facter/facts.d':
-          ensure => 'directory',
-          owner  => 'root',
-          group  => 'root',
-          mode   => '0755',
-     }
-     file { '/etc/puppetlabs/facter/facts.d/facts.yaml':
-          ensure => 'present',
-          owner  => 'root',
-          group  => 'root',
-          mode   => '0755',
-     }
+  if $::role {
+    notify { "This is picking up the $::role role": }
+    include "role::${::role}"
+  }
+  else {
+  notify { 'This is from site.pp there is no role facter':}
+  include role::default
+  }
+
 }
 
