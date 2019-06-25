496163db8c4cab84323d8f5a3a2073c25fb670ba
Barun <barun.mittal@capgemini.com>
2019-06-25 10:31:28 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,5 +1,5 @@
 node 'puppetagent.master.com' {
-    notify { "This is picking up the $::role role": }
-    include "role::${::role}"
+    notify { "This is picking up the big role": }
+    include "role::big"
 }
 
