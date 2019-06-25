c2f634b2f2ebb5665a1df3d616d97709cca377ba
Barun <barun.mittal@capgemini.com>
2019-06-25 10:29:50 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,12 +1,5 @@
 node 'puppetagent.master.com' {
-  if $::role {
     notify { "This is picking up the $::role role": }
     include "role::${::role}"
-  }
-  else {
-  notify { 'This is from site.pp there is no role facter':}
-  include role::default
-  }
-
 }
 
